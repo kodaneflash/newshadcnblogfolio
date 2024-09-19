d5ccb339119c0f95ed7a7d543101c5a108c486e8
atlasintelligencellc <atlasintelligencellc@gmail.com>
2024-09-18 21:30:29 -0400

    e

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -29,11 +29,11 @@ export default function Home() {
   return (
     <>
       <BackgroundLines>
-        <div className="flex flex-col items-center justify-center min-h-screen w-full text-center px-4 sm:px-6 lg:px-8">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight max-w-3xl mx-auto bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        <div className="flex flex-col items-center justify-center min-h-screen w-full text-center px-4 sm:px-6 lg:px-8 -mt-80 sm:-mt-28 md:-mt-32"> {/* Doubled negative margin for mobile */}
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight max-w-3xl mx-auto bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 pb-2 sm:pb-6">
             I'm a solopreneur, builder and founder.
           </h1>
-          <p className="text-lg sm:text-xl md:text-2xl text-neutral-500 dark:text-neutral-400 max-w-3xl mx-auto mt-4">
+          <p className="text-lg sm:text-xl md:text-2xl text-neutral-500 dark:text-neutral-400 max-w-3xl mx-auto mt-1 sm:mt-4">
             I'm a serial entrepreneur, leader, and strategist. I've founded 6 online software companies, with 2 being acquired. I have a unique ability to manage multi-disciplinary projects and navigate complex challenges. I share my knowledge and experience on building successful online businesses.
           </p>
         </div>
