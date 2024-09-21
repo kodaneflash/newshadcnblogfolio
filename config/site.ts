963b2ceaac8d4647ecfb83a3ef5cabda8432fd97
atlasintelligencellc <atlasintelligencellc@gmail.com>
2024-09-20 23:07:14 -0400

    ee

--- config/site.ts
+++ config/site.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
   name: "James Archer | Founder | Solo Entrepreneur",
   title: "I'm a solopreneur, builder and founder.",
   description:
-    "I’m a serial entrepreneur, leader, and strategist. I've founded 6 online software companies, with 2 being acquired. I teach solo entrepreneurs how to build and scale successful online businesses.",
+    "I've founded 6 online software companies, with 2 being acquired. I teach solo entrepreneurs how to build and scale successful online businesses.",
   url: "",
   ogImage: "/og.jpg",
   links: {
